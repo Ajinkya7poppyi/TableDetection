0dc368e63c58b534815f14a865f68a25ebe9f20d
Ajinkya Patil <san.avpatil@gmail.com>
2021-10-19 15:20:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ python -m pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wh
 pip install -r requirements.txt
 echo "Downloading Table Detection Models"
 gdown --id 11FgFTy0MyVUMGd00T_InEDaarB4qAlP8 -O content/
-gdown --id 1WBk6kHHyvyEzoPBsRr2BvFY51zURjd4R -O content/
+gdown --id 1bXOfgIt9ALCAlJkfXWG2YANLJgvTwQWU -O content/
 gdown --id 1PfA2uws919gc893-x9uMIz06zWEko8nF -O content/
 echo "Setup Complete. Please run below command to start execution of server."
 echo "steamlit run app.py"
